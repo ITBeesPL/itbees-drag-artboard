549311fd7068afddcaf6a3b2b76b9f5db838dbae
Grzegorz Serwin <grzegorz@itbees.pl>
2024-01-20 14:18:24 +0100

    toolbox

--- src/app/artboard/artboard/artboard.component.ts
+++ src/app/artboard/artboard/artboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ToolboxComponent } from '../toolbox/toolbox.component';
 
 @Component({
   selector: 'app-artboard',
   standalone: true,
-  imports: [],
+  imports: [ToolboxComponent],
   templateUrl: './artboard.component.html',
   styleUrl: './artboard.component.scss'
 })
