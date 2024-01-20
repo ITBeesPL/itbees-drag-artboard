08cc4cf274bd8defe8788dfbaad77ca2977db489
Grzegorz Serwin <grzegorz@itbees.pl>
2024-01-20 13:56:22 +0100

    basic grid

---
+++ src/app/artboard/artboard/artboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './artboard.component.html',
+  styleUrl: './artboard.component.scss'
+})
+export class ArtboardComponent {
+
+}
